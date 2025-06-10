b31ee835b130dcbda7d09ce34de7d69b1fc7895f
ohzqq <iamchurkey@gmail.com>
2025-06-10 10:47:36 -0400

    cropping works

---
+++ crop_test.go
@@ -0,0 +1,28 @@
+package imgconv
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+)
+
+func TestCrop(t *testing.T) {
+	opts := []*CropOption{
+		&CropOption{
+			Width:  350,
+			Height: 350,
+			Anchor: Center,
+		},
+	}
+	over, err := Open("testdata/soft0000.tif")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i, opt := range opts {
+		img := Crop(over, opt)
+		err := Save(filepath.Join("testdata", fmt.Sprintf("test-%d.tiff", i)), img, &FormatOption{})
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
